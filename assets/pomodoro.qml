25fde724d3b81a1a30b16e32328b9fb771e48ec7
Romeo Disca <romeo.disca@gmail.com>
2014-12-18 18:59:52 +0100

    full functioning pomodoro timer

--- assets/pomodoro.qml
+++ assets/pomodoro.qml
@@ -1,55 +1,81 @@
 import QtQuick 2.2
 import QtMultimedia 5.0
 
-Image {
-  id: pImg
-
-  width: 200
-  height: 200
-  rotation: 0
-
-  source: 'pomodoro.png'
-
-  state: { return pomodoro.isRinging ? 'ringing' : 'normal' }
-  states: [
-    State {
-      name: 'normal'
-      PropertyChanges {
-        target: pImg
-        rotation: 0
+Column {
+  Rectangle {
+    width: 200
+    height: 50
+    color: '#cccccc'
+    Text {
+      anchors {
+        fill: parent.fill
+        horizontalCenter: parent.horizontalCenter
+        verticalCenter: parent.verticalCenter
       }
-    },
-    State {
-      name: 'ringing'
-    }
-  ]
-
-  transitions: [
-    Transition { 
-      to: 'normal'
-      RotationAnimation { target: pImg; property: 'rotation'; duration: 50; to: 0 }
-    },
-    Transition { 
-      to: 'ringing'
-      SequentialAnimation { 
-        id: ringingAnim
-        loops: Animation.Infinite
-        RotationAnimation { target: pImg; property: 'rotation'; duration: 50; to: 10 }
-        RotationAnimation { target: pImg; property: 'rotation'; duration: 50; to: -10 }
+      text: pomodoro.countdown
+      color: 'black'
+      font {
+        family: 'Arial'
+        pointSize: 24
+        bold: true
       }
     }
-  ]
-
-  SoundEffect {
-    objectName: 'tick'
-    id: tick
-    source: 'tick.wav'
   }
+  Image {
+    id: pImg
 
-  SoundEffect {
-    objectName: 'ring'
-    id: ring
-    source: 'ring.wav'
+    width: 200
+    height: 200
+    rotation: 0
+
+    source: 'pomodoro.png'
+
+    state: { return pomodoro.isRinging ? 'ringing' : 'normal' }
+    states: [
+      State {
+        name: 'normal'
+        PropertyChanges {
+          target: pImg
+          rotation: 0
+        }
+      },
+      State {
+        name: 'ringing'
+      }
+    ]
+
+    MouseArea {
+      objectName: 'mouseArea'
+      anchors.fill: parent
+    }
+
+    transitions: [
+      Transition { 
+        to: 'normal'
+        RotationAnimation { target: pImg; property: 'rotation'; duration: 50; to: 0 }
+      },
+      Transition { 
+        to: 'ringing'
+        SequentialAnimation { 
+          id: ringingAnim
+          loops: Animation.Infinite
+          RotationAnimation { target: pImg; property: 'rotation'; duration: 50; to: 10 }
+          RotationAnimation { target: pImg; property: 'rotation'; duration: 50; to: -10 }
+        }
+      }
+    ]
+
+    SoundEffect {
+      objectName: 'tick'
+      id: tick
+      source: 'tick.wav'
+    }
+
+    SoundEffect {
+      objectName: 'ring'
+      id: ring
+      source: 'ring.wav'
+    }
+
   }
-
 }
--- main.go
+++ main.go
@@ -8,35 +8,98 @@ import (
   "gopkg.in/qml.v1"
 )
 
+const (
+  Stop = iota
+  Start
+  Ring
+)
+
 type Pomodoro struct {
   IsRinging bool
+  isTicking bool
   ringing qml.Object
   ticking qml.Object
+  Countdown string
+  timer int32
+  timerDefault int32
+  event chan int8
+}
+
+func NewPomodoro() *Pomodoro {
+  out := &Pomodoro{ timerDefault: 25 * 60 }
+  out.Reset()
+  out.event = make(chan int8)
+  return out
 }
 
 func (p *Pomodoro) Run() {
 
+  p.timerDefault = 10
   go func() {
-    ticktack := func() {
-      p.ticking.Set("loops", 1 << 10)
-      p.ticking.Call("play")
-    }
-    ticktack()
-    for {
-      time.Sleep(time.Second * 3)
-      p.IsRinging = !p.IsRinging
-      if (p.IsRinging) {
-        p.ticking.Call("stop")
-        p.ringing.Set("loops", 1 << 10)
-        p.ringing.Call("play")
-      } else {
-        p.ringing.Call("stop")
-        ticktack()
+    p.event <- Stop
+  }()
+
+  go func() {
+    for event := range p.event {
+      switch event {
+        case Start:
+          p.Reset()
+          p.ticking.Set("loops", 1 << 10)
+          p.ticking.Call("play")
+          p.IsRinging = true
+          p.isTicking = true
+          go func() {
+            for p.isTicking {
+              time.Sleep(time.Second)
+              if p.isTicking && p.decTimer().Timer() == 0 {
+                p.event <- Ring
+              }
+            }
+          }()
+        case Stop:
+          p.Reset()
+          p.ticking.Call("stop")
+          p.ringing.Call("stop")
+          p.IsRinging = false
+          p.isTicking = false
+        case Ring:
+          p.ringing.Set("loops", 1 << 10)
+          p.ringing.Call("play")
+          p.IsRinging = true
+          p.isTicking = false
       }
-      qml.Changed(p, &p.IsRinging)
     }
   }()
 
+}
+func (p *Pomodoro) ToggleRun() {
+  if p.IsRinging || p.isTicking {
+    p.event <- Stop
+  } else if !p.isTicking {
+    p.event <- Start
+  }
+}
+
+func (p *Pomodoro) SetTimerDefault(timer int32) *Pomodoro {
+  p.timerDefault = timer
+  return p
+}
+func (p *Pomodoro) setTimer(timer int32) *Pomodoro {
+  p.timer = timer
+  p.Countdown = fmt.Sprintf("%02d:%02d", p.timer / 60, p.timer % 60)
+  qml.Changed(p, &p.Countdown)
+  return p
+}
+func (p *Pomodoro) Timer() int32 {
+  return p.timer
+}
+func (p *Pomodoro) decTimer() *Pomodoro {
+  p.setTimer(p.Timer() - 1)
+  return p
+}
+func (p *Pomodoro) Reset() *Pomodoro {
+  p.setTimer(p.timerDefault)
+  return p
 }
 
 func main() {
@@ -54,15 +117,19 @@ func run() error {
     return err
   }
 
-  pomo := Pomodoro{}
+  pomo := NewPomodoro()
 
   context := engine.Context()
-  context.SetVar("pomodoro", &pomo)
+  context.SetVar("pomodoro", pomo)
 
   win := component.CreateWindow(nil)
 
   pomo.ticking = win.Root().ObjectByName("tick")
   pomo.ringing = win.Root().ObjectByName("ring")
+  mouse := win.Root().ObjectByName("mouseArea")
+  mouse.On("clicked", func(event qml.Object) {
+    pomo.ToggleRun();
+  })
 
   pomo.Run()
 
