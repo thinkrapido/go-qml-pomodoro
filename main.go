f168b6e6ab32c7e44f992ed1b63ab0c39ecdf450
Romeo Disca <romeo.disca@gmail.com>
2015-02-26 07:13:48 +0100

    using go-statemachine

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
   "time"
 
   "gopkg.in/qml.v1"
+
+  state "github.com/thinkrapido/go-statemachine/state"
 )
 
 const (
@@ -15,72 +17,97 @@ const (
 )
 
 type Pomodoro struct {
-  IsRinging bool
-  isTicking bool
+  state.Machine
+  
   ringing qml.Object
   ticking qml.Object
+  
   Countdown string
+  
   timer int32
   timerDefault int32
-  event chan int8
+
+  IsRinging bool
 }
 
 func NewPomodoro() *Pomodoro {
   out := &Pomodoro{ timerDefault: 25 * 60 }
-  out.Reset()
-  out.event = make(chan int8)
+  out.init()
   return out
 }
 
-func (p *Pomodoro) Run() {
+func (p *Pomodoro) init() {
+
+  p.Machine.Init()
+
+  p.Learn("waiting", "ticking", "click", p.startClock)
+  p.Learn("ticking", "waiting", "click", p.resetClock)
+
+  p.Learn("ticking", "ticking", "tick", p.tick)
+  p.Learn("ticking", "ringing", "timeout", p.startRinging)
+  p.Learn("ringing", "waiting", "click", p.stopRinging)
+
+  p.SetStartState("waiting")
+  p.AddListener(p)
 
   p.timerDefault = 10
-  go func() {
-    p.event <- Stop
-  }()
-
-  go func() {
-    for event := range p.event {
-      switch event {
-        case Start:
-          p.Reset()
-          p.ticking.Set("loops", 1 << 10)
-          p.ticking.Call("play")
-          p.IsRinging = false
-          p.isTicking = true
-          qml.Changed(p, &p.IsRinging)
-          go func() {
-            for p.isTicking {
-              time.Sleep(time.Second)
-              if p.isTicking && p.decTimer().Timer() == 0 {
-                p.event <- Ring
-              }
-            }
-          }()
-        case Stop:
-          p.Reset()
-          p.ticking.Call("stop")
-          p.ringing.Call("stop")
-          p.IsRinging = false
-          p.isTicking = false
-          qml.Changed(p, &p.IsRinging)
-        case Ring:
-          p.ringing.Set("loops", 1 << 10)
-          p.ringing.Call("play")
-          p.IsRinging = true
-          p.isTicking = false
-          qml.Changed(p, &p.IsRinging)
-      }
-    }
-  }()
+  p.Reset()
 
 }
-func (p *Pomodoro) ToggleRun() {
-  if p.IsRinging || p.isTicking {
-    p.event <- Stop
-  } else if !p.isTicking {
-    p.event <- Start
+
+func (p *Pomodoro) Notify(e *state.Event) {
+  switch e.Event {
+    case state.StateReachedEvent:
+      p.IsRinging = false
+      switch p.CurrentState() {
+        case "ticking":
+          if p.Timer() == 0 {
+            p.Trigger("timeout")
+          } else {
+            time.Sleep(time.Second)
+            p.Trigger("tick")
+          }
+        case "ringing":
+          p.IsRinging = true
+      }
+      qml.Changed(p, &p.IsRinging)
+    case state.InconsistencyEvent:
+      fmt.Printf("state inconsistent: %s\n", e.Message)
+    case state.KillEvent:
+      p.Run()
   }
+}
+
+func (p *Pomodoro) startClock() {
+  p.Reset()
+  p.ticking.Set("loops", 1 << 10)
+  p.ticking.Call("play")
+}
+
+func (p *Pomodoro) resetClock() {
+  p.Reset()
+  p.ticking.Call("stop")
+}
+
+func (p *Pomodoro) tick() {
+  p.decTimer()
+}
+
+func (p *Pomodoro) startRinging() {
+  p.ticking.Call("stop")
+  p.ringing.Set("loops", 1 << 10)
+  p.ringing.Call("play")
+}
+
+func (p *Pomodoro) stopRinging() {
+  p.Reset()
+  p.ticking.Call("stop")
+  p.ringing.Call("stop")
+}
+
+func (p *Pomodoro) Reset() *Pomodoro {
+  p.setTimer(p.timerDefault)
+  return p
 }
 
 func (p *Pomodoro) SetTimerDefault(timer int32) *Pomodoro {
@@ -98,10 +125,6 @@ func (p *Pomodoro) Timer() int32 {
 }
 func (p *Pomodoro) decTimer() *Pomodoro {
   p.setTimer(p.Timer() - 1)
-  return p
-}
-func (p *Pomodoro) Reset() *Pomodoro {
-  p.setTimer(p.timerDefault)
   return p
 }
 
@@ -131,7 +154,7 @@ func run() error {
   pomo.ringing = win.Root().ObjectByName("ring")
   mouse := win.Root().ObjectByName("mouseArea")
   mouse.On("clicked", func(event qml.Object) {
-    pomo.ToggleRun();
+    pomo.Trigger("click");
   })
 
   pomo.Run()
